ccfd9d68e65661e02497bf38b57a219c6a882537
John Rey <adioskid@mail.ru>
2022-10-16 16:36:06 +0300

    Homework#2

--- src/TermsThree.java
+++ src/TermsThree.java
@@ -1,9 +1,9 @@
 public class TermsThree {
     public static void main(String[] args) {
-        int year = 4;
+        int year = 2004;
         String text = " год является високосным.";
         String textOther = " год не является високосным.";
-        if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 400 == 0) && (year % 100 == 0))) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println(year + text);
         } else {
             System.out.println(year + textOther);
