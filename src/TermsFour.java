38893c04ae7f11904da6d3ed710d30b86c7b0fa8
John Rey <adioskid@mail.ru>
2022-10-27 12:38:41 +0300

    Homework#2

--- src/TermsFour.java
+++ src/TermsFour.java
@@ -2,15 +2,16 @@ public class TermsFour {
     public static void main(String[] args) {
         int deliveryDistance = 95;
         int deliveryDays = 1;
-
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            deliveryDays += 1;
-            System.out.println("Потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            deliveryDays += 2;
-            System.out.println("Потребуется дней: " + deliveryDays);
+        int interval = 40;
+        int firstDayDistance = 20;
+        if ((deliveryDistance % interval < firstDayDistance) && interval >= firstDayDistance) {
+            deliveryDays += deliveryDistance / interval;
+            System.out.println(deliveryDays);
+        } else if (interval < firstDayDistance) {
+            System.out.println("Ошибка! Интервал доставки не может быть меньше интервала первого дня");
+        } else {
+            deliveryDays += deliveryDistance / interval + 1;
+            System.out.println(deliveryDays);
         }
     }
 }
